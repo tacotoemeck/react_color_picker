91612fafb69092131d128a7b50e42ddada2ad013
Tomek Wagner <wagner.tomek@gmail.com>
2020-02-14 12:26:41 +0000

    created react app and added base files

---
+++ src/Pallet.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Palette extends React.Component {
+    render() {
+        return (
+            <div className="Palette">
+                {/* navbar goes here */}
+                <div className="Palette-colors">
+                    {/* color boxes go here */}
+                </div>
+                {/* footer here */}
+            </div>
+        )
+    }
+}
+
+export default Palette;
