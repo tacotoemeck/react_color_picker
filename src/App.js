d25abb3207e8c0580ca97c12bc376da9190f0125
Tomek Wagner <wagner.tomek@gmail.com>
2020-02-14 14:21:31 +0000

    added color helper npm package to generate new colors shades and varios color groups

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Palette from './Pallet'
 import seedColors from './seedColors';
+import { generatePalette } from './ColorHelpers';
 import './App.css';
 
 class App extends React.Component {
   render() {
+    console.log(generatePalette(seedColors[4]))
     return (
       <div>
         <Palette {...seedColors[2]} />
