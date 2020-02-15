4da181022b41aaec2e666dbadf2ad2b76da479b3
Tomek Wagner <wagner.tomek@gmail.com>
2020-02-15 19:56:29 +0000

    added PalletList with links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Palette from './Pallet'
+import Palette from './Pallet';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
 import './App.css';
@@ -12,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' render={() => <h1>pallets</h1>} />
+        <Route exact path='/' render={() => <PaletteList palettes={seedColors} />} />
         <Route
           exact
           path='/palette/:id'
