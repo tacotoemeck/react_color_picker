a5ac46881b7bf164b5abd28c92848dd00a3dd036
Tomek Wagner <wagner.tomek@gmail.com>
2020-02-15 18:59:53 +0000

    added react-router with some trial routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Pallet'
 import seedColors from './seedColors';
 import { generatePalette } from './ColorHelpers';
@@ -7,9 +8,14 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[0])} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>pallets</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>palleasdasdasdts</h1>} />
+      </Switch>
+
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[0])} />
+      // </div>
 
     )
   }
