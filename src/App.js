f0ccc92db8329cb0a61e4f991908c6dc0cd4edd7
Tomek Wagner <wagner.tomek@gmail.com>
2020-02-19 12:22:34 +0000

    added a NewPaletteForm

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Palette from './Pallet';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
+import NewPaletteForm from './NewPaletteForm';
 import { generatePalette } from './ColorHelpers';
 import './App.css';
 
@@ -14,6 +15,7 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route exact path='/palette/new' render={() => <NewPaletteForm />} />
         <Route
           exact
           path='/'
---
+++ src/NewPaletteForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NewPaletteForm extends React.Component {
+    render() {
+        return (
+            <h1>New Palette Form</h1>
+        )
+    }
+}
+
+export default NewPaletteForm;
