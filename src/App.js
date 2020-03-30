db1ee7ac1885e2dd4282a1827f73eee7e0e7cfd8
tacotoemeck <wagner.tomek@gmail.com>
2020-03-30 08:52:11 +0100

    Add delete MiniPalette funcionality

--- src/App.js
+++ src/App.js
@@ -17,9 +17,18 @@ class App extends React.Component {
     this.state = { palettes: savedPalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find(palette => palette.id === id);
+  }
+  deletePalette(id) {
+    this.setState(
+      state => ({
+        palettes: state.palettes.filter(palette => palette.id !== id),
+      }),
+      this.syncLocalStorage,
+    );
   }
   savePalette(newPalette) {
     this.setState(
@@ -54,6 +63,7 @@ class App extends React.Component {
           render={routeProps => (
             <PaletteList
               palettes={this.state.palettes}
+              deletePalette={this.deletePalette}
               {...routeProps}
             />
           )}
@@ -82,10 +92,6 @@ class App extends React.Component {
           )}
         />
       </Switch>
-
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[0])} />
-      // </div>
     );
   }
 }
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,29 +5,34 @@ import { withStyles } from '@material-ui/styles';
 import styles from './styles/PaletteListStyles';
 
 class PaletteList extends React.Component {
-    goToPalette(id) {
-        this.props.history.push(`/palette/${id}`)
-    }
-    render() {
-        const { palettes, classes } = this.props;
-        return (
-            <div className={classes.root}>
-                <div className={classes.container}>
-                    <nav className={classes.nav}>
-                        <h1>React Colors</h1>
-                        <Link to='./palette/new'>Create Palette</Link>
-                    </nav>
-                    <div className={classes.palettes}>
-                        {palettes.map(palette => (
-                            <MiniPalette {...palette}
-                                handleClick={() => this.goToPalette(palette.id)} />
-                        ))
-                        }
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
+  render() {
+    const { palettes, classes, deletePalette } = this.props;
+    console.log(deletePalette);
+    return (
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React Colors</h1>
+            <Link to="./palette/new">Create Palette</Link>
+          </nav>
+          <div className={classes.palettes}>
+            {palettes.map(palette => (
+              <MiniPalette
+                {...palette}
+                handleClick={() => this.goToPalette(palette.id)}
+                handleDelete={deletePalette}
+                key={palette.id}
+                id={palette.id}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(PaletteList);
